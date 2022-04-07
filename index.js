fa7069492ba40b67a472e27384a35097b5c56d5f
Lyaon <lyaonplayz@gmail.com>
2022-04-06 17:32:08 -0700

    Command Handler for v13

--- index.js
+++ index.js
@@ -3,17 +3,16 @@ const token = require("./Token.js");
 const { Client, Intents } = require('discord.js');
 const allIntents = new Intents(32767);
 const client = new Discord.Client({ intents: allIntents });
-const Discord = require('discord.js');
-const client = new Discord.Client({ intents: ['DIRECT_MESSAGES', 'GUILDS', 'GUILD_BANS', 'GUILD_MEMBERS', 'GUILD_MESSAGES', 'GUILD_WEBHOOKS'], partials: ['CHANNEL', 'MESSAGE'] });
-const prefix = '?'
 const fs = require('fs')
 
+client.on('ready', () => {
+    console.log(`Bot is online!`);
+  client.user.setActivity('Lyaon Suffer', { type: 'WATCHING'});
+});
+
+// Command Handler
 client.commands = new Discord.Collection();
-
-// Filter Commands //
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-
-// Fetch Commands //
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
 
@@ -22,11 +21,9 @@ for (const file of commandFiles) {
 
 client.on('ready', () => {
   	console.log(`Bot is online!`);
-  	client.user.setPresence({ activities: [{ name: 'with commands' }] });
+    client.user.setActivity('Lyaon Suffer', { type: 'WATCHING'});
 });
-
 client.on('messageCreate', async message => {
-    // Define Command Components //
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase()
 
@@ -34,9 +31,5 @@ client.on('messageCreate', async message => {
         client.commands.get('help').execute(message)
     }
 })
-
-client.once('ready', () => {
-    console.log('Ready to Roll!');
-});
 
 client.login(key);
