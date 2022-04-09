281969eac72da47030aee959de186bf80cde9e92
Lyaon <lyaonplayz@gmail.com>
2022-04-08 19:38:55 -0700

    Working Command Handler
    Found a better command handler that supports prefix and slash commands. Credits to https://github.com/reconlx/djs-base-handler for base code!

--- README.md
+++
@@ -1,2 +0,0 @@
-# Lyaon-Bot
-A Discord.JS v13 discord bot for all of Lyaon's needs.
--- index.js
+++ index.js
@@ -1,34 +1,26 @@
-const Discord = require('discord.js');
-const fs = require('fs')
-const prefix = '?';
-const client = new Discord.Client(32767);
-const allIntents = Discord.Intents.all();
-const bot = new Discord.Client({ intents: Discord.Intents.ALL });
+const { Client, Collection } = require("discord.js");
+const client = new Client({ intents: 131071 });
+module.exports = client;
+const Color = require('color');
+const db = require("quick.db");
+const path = require("path");
+const fs = require("fs");
+const ms = require("ms");
+const BadWords = require("./BadWords.js");
+const System = require("./System.js");
 const token = require("./Token.js");
- 
-client.commands = new Discord.Collection();
- 
-const commandFiles = fs.readdirSync('./commands/bot').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
-    const command = require(`./commands/bot/${file}`);
- 
-    client.commands.set(command.name, command);
-}
- 
+
 client.once('ready', () => {
     console.log('Ready!');
-    client.user.setActivity('Torturing Lyaon', { type: 'STREAMING'});
-});
- 
-client.on("messageCreate", message => { //Changed message to messageCreate
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
- 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
- 
-    if(command === 'ping'){
-        client.commands.get('ping').execute(message, args);
-    };
-});
- 
+    client.user.setActivity('Torturing Humans', { type: 'STREAMING'});
+  });
+
+// Global Variables
+client.commands = new Collection();
+client.slashCommands = new Collection();
+client.config = require("./config.json");
+
+// Initializing the project
+require("./handler")(client);
+
 client.login(key);
